58d43825844a6446f00a72da07dd4a699ae72793
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2014-09-20 13:16:22 +0000

    added google Analytics tracker

--- app/views/static/template_simple_photobackground.blade.php
+++ app/views/static/template_simple_photobackground.blade.php
@@ -9,7 +9,7 @@
 
 <body>
 
-<div id="pageWrapper">
+<div id="pageWrapper" class="dark" data-image="{{asset('img/lb-background-huge.jpg')}}">
   <header>
     <div class="inner">
     <a href="{{URL::to('/posts/all')}}"><div id="logo">
@@ -18,48 +18,32 @@
     </div>
   </header>
   <div id="contentWrapper" class="inner">
-      <aside>
-        <ul>
-          <a href="{{URL::to('/about')}}"
-          <?php if (empty($slug)) {
-            echo 'class="active" ';} ?>>
-            <li>About</li>
-          </a>
-          <a href="{{URL::to('/about/faq')}}"
-          <?php if ($slug == 'faq') {
-            echo 'class="active" ';} ?>>
-            <li>Frequently Asked Question</li>
-          </a>
-          <a href="{{URL::to('/about/submit')}}"
-          <?php if ($slug == 'submit') {
-            echo 'class="active" ';} ?>>
-            <li>Submit Your Blog</li>
-          </a>
-          <a href="{{URL::to('/about/feedback')}}"
-          <?php if ($slug == 'feedback') {
-            echo 'class="active" ';} ?>>
-            <li>Submit Feedback</li>
-          </a>
-          <a href="{{URL::to('/about/badge')}}"
-          <?php if ($slug == 'badge') {
-            echo 'class="active" ';} ?>>
-            <li>Add our Badge to your blog</li>
-          </a>
-        </ul>
-      </aside>
-
-      <div class="content">
+      <div class="simple_content">
         @yield('content')
       </div>
-    </div>
-  <footer>
+  </div>
+  <footer class="dark">
     <div class="inner">
       <?php echo 'Lebanese Blogs ' . date('Y') . ' &copy;'; ?>
     </div>
   </footer>
-</div> <!-- /pageWrapper -->
+</div>
 
-          <!-- Start of Google Analytics Code -->
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src ="{{asset('js/third_party/jquery.backstretch.min.js')}}"></script>
+<script>
+
+  $(document).ready(function(){
+
+      $('#pageWrapper').backstretch($('#pageWrapper').data('image'));
+
+  });
+
+
+</script>
+  <!-- /pageWrapper -->
+
+  <!-- Start of Google Analytics Code -->
             <script>
               (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
               (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -86,6 +70,6 @@
             class="statcounter"
             src="https://c.statcounter.com/8489889/0/6ec3dc93/1/"
             alt="web counter"></a></div></noscript>
-        <!-- End of StatCounter Code for Default Guide -->
+  <!-- End of StatCounter Code for Default Guide -->
 </body>
 </html>
