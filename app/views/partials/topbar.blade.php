c928d0d619064867a281bebb27d51299e71dfe77
Mustapha <mustapha.hamoui@gmail.com>
2014-09-21 02:40:37 +0000

    added links for dropdown menu

--- app/views/partials/topbar.blade.php
+++ app/views/partials/topbar.blade.php
@@ -12,10 +12,10 @@
 		<ul>
 			<li><a href="{{URL::to('/about')}}">What is this Website?</a></li>
 			<li><a href="{{URL::to('/about/faq')}}">Frequently Asked Question</a></li>
-      <li><a href="">Submit Your Blog</a></li>
-			<li><a href="">Submit Feedback</a></li>
-			<li><a href="">Our own Blog</a></li>
-			<li><a href="">Admin</a></li>
+      <li><a href="{{URL::to('/about/submit')}}">Submit Your Blog</a></li>
+			<li><a href="{{URL::to('/about/feedback')}}">Submit Feedback</a></li>
+			<li><a href="http://blog.lebaneseblogs.com">Our own Blog</a></li>
+			<li><a href="#">Admin</a></li>
 		</ul>
 	</div>
 </div>
