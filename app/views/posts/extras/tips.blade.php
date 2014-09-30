8e64ef93086e29055b76ca5f8f2250e629e07e53
Mustapha <mustapha.hamoui@gmail.com>
2014-09-30 20:05:03 +0000

    added new poster for tips

--- app/views/posts/extras/tips.blade.php
+++ app/views/posts/extras/tips.blade.php
@@ -1,5 +1,5 @@
 <div class="post_wrapper no_min">
-  <?php $randomKey = rand(1,3); ?>
+  <?php $randomKey = rand(1,4); ?>
   <div class="card no_color no_border no_min">
     @if ($randomKey == 1)
       <img src="{{asset('img/poster_favorites.png')}}" width="300" height="300" alt="">
@@ -7,6 +7,8 @@
       <img src="{{asset('img/poster_saved.png')}}" width="300" height="300" alt="">
     @elseif ($randomKey == 3)
       <img src="{{asset('img/poster_twitter.png')}}" width="300" height="300" alt="">
+    @elseif ($randomKey == 4)
+      <img src="{{asset('img/poster_edit_your_blog.png')}}" width="300" height="300" alt="">
     @endif
   </div>
 </div>
