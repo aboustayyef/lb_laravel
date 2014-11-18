4f77d99a377ee0c2c216854ff31daf344ce4972b
Mustapha <mustapha.hamoui@gmail.com>
2014-11-18 17:54:02 +0000

    force css to load

--- app/views/partials/header.blade.php
+++ app/views/partials/header.blade.php
@@ -38,7 +38,7 @@
     <link rel="apple-touch-icon" sizes="180x180" href="{{asset('/img/apple-touch-icons/180x180.png')}}">
 
     <!-- Style Sheet -->
-    <link rel="stylesheet" href="{{asset('/css/lebaneseblogs.css?v=2.12')}}">
+    <link rel="stylesheet" href="{{asset('/css/lebaneseblogs.css?v=2.121')}}">
 
     <!-- Favicon -->
     <link rel="shortcut icon" href="{{asset('/img/favicon.ico')}}" >
