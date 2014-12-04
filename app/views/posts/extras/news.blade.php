f8dcb8c312125387b8d0784bc844a47ea84e4a82
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2014-12-04 09:43:57 +0000

    beta of lebanese latest news ready for publishing

--- app/views/posts/extras/news.blade.php
+++ app/views/posts/extras/news.blade.php
@@ -6,20 +6,19 @@ if (!Cache::has('naharnet')) {
 <div class="post_wrapper">
   <div class="card news">
     <div class="newsheader">
-      <h3>Naharnet News <span class="beta">(beta)</span></h3>
+      <h3>Latest Lebanon News <span class="beta">(beta)</span><small>Source: Naharnet</small></h3>
     </div>
     <?php
       $articles = Cache::get('naharnet');
       $articles = array_chunk($articles, 5);
       $articles = $articles[0];
-
     ?>
     <ul>
       @foreach($articles as $article)
       <?php $virality = $article['virality']; ?>
         <li>
           <div class="newsitem">
-            <a href="{{$article['url']}}">{{$article['headline']}}</a>
+            <a href="{{$article['url']}}" target="blank">{{$article['headline']}}</a>
             {{View::make('posts.partials.virality')->with('score',$virality)}}
           </div>
         </li>
