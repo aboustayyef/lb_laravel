abec8defef73a7a2df3433ffd53bd3052c7771dd
Mustapha <mustapha.hamoui@gmail.com>
2014-12-18 19:58:57 +0000

    countdown ready to be published

--- app/views/posts/extras/2014countdown.blade.php
+++ app/views/posts/extras/2014countdown.blade.php
@@ -2,20 +2,23 @@
   <div class="card no_color no_border no_min" style="height:350px;width:300px">
     <img src ="{{asset('img/top_lists_teaser_2_600x700.png')}}" height="350px" width="300px">
 
-    <div id="list_countdown" style="
-      position: absolute;
-      left: 41px;
-      bottom: 40px;
-      color: white;
-      font-size: 30px;">
-    </div>
-    <div style="position: absolute;
-      bottom: 10px;
-      left: 50px;
-      color: white;
-      opacity:0.8;
-      font-size: 12px;">
-      days&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;hours&nbsp;&nbsp;&nbsp;&nbsp;minutes&nbsp;&nbsp;&nbsp;&nbsp;seconds
+    <div id="list_countdown">
+      <div class="block">
+        <div class="number days"></div>
+        <div class="description">Days</div>
+      </div>
+      <div class="block">
+        <div class="number hours"></div>
+        <div class="description">Hours</div>
+      </div>
+      <div class="block">
+        <div class="number mins"></div>
+        <div class="description">Mins</div>
+      </div>
+      <div class="block">
+        <div class="number secs"></div>
+        <div class="description">Secs</div>
+      </div>
     </div>
   </div>
 </div>
--- public/js/lb.sources/lb.teasercountdown.js
+++ public/js/lb.sources/lb.teasercountdown.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
   $('#list_countdown')
   .countdown("2014/12/22 08:00:00", function(event){
-    $(this).text(
-      event.strftime('%d  |  %H  |  %M  |  %S')
-    );
+    // $(this).text(
+    //   event.strftime('%d  |  %H  |  %M  |  %S')
+    // );
+    $(this).find('.block').find('.days').text(event.strftime('%d'));
+    $(this).find('.block').find('.hours').text(event.strftime('%H'));
+    $(this).find('.block').find('.mins').text(event.strftime('%M'));
+    $(this).find('.block').find('.secs').text(event.strftime('%S'));
   });
 });
