3a0724e4070b3878acdd9f4a391a8c070fb82e74
Mustapha <mustapha.hamoui@gmail.com>
2014-12-21 20:50:10 +0000

    ready to deploy stats2014

--- app/views/posts/extras/2014countdown.blade.php
+++ app/views/posts/extras/2014countdown.blade.php
@@ -1,8 +1,8 @@
 <div class="post_wrapper no_min">
-  <div class="card no_color no_border no_min" style="height:350px;width:300px">
-    <img src ="{{asset('img/top_lists_teaser_2_600x700.png')}}" height="350px" width="300px">
+  <div class="card no_color no_border no_min" style="height:536;width:300px">
+    <a href="http://stats2014.lebaneseblogs.com"><img src ="{{asset('img/stats_now_live.png')}}" height="536px" width="300px"></a>
 
-    <div id="list_countdown">
+    <!-- <div id="list_countdown">
       <div class="block">
         <div class="number days"></div>
         <div class="description">Days</div>
@@ -19,6 +19,6 @@
         <div class="number secs"></div>
         <div class="description">Secs</div>
       </div>
-    </div>
+    </div> -->
   </div>
 </div>
--- public/js/lebaneseblogs.js
+++ public/js/lebaneseblogs.js
@@ -2,7 +2,3 @@
 // remember to remove jquery once we're using cdn
 // @codekit-prepend "third_party/jquery-1.11.0.min.js", "third_party/jquery.backstretch.min.js", "third_party/masonry.pkgd.min.js"
 // @codekit-append "lb.sources/lb.events.js", "lb.sources/lb.favorites.js", "lb.sources/lb.functions.js", "lb.sources/lb.menus.js", "lb.sources/lb.preparefonts.js", "lb.sources/lb.saved.js", "lb.sources/lb.toplist.js"
-
-// remove countdown after publishing 2014 results
-// @codekit-append "lb.sources/lb.teasercountdown.js"
-// @codekit-append "third_party/jquery.countdown.min.js"
