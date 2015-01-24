0da684556b2e68db7609333ae9851a16ea1bd44e
Mustapha <mustapha.hamoui@gmail.com>
2015-01-24 07:15:56 +0000

    converted rating to a 5 star system

--- app/views/posts/partials/rating.blade.php
+++ app/views/posts/partials/rating.blade.php
@@ -1,21 +1,32 @@
 {{--
 
-Requires:
+Requires the original numerator and denominator:
 Numerator: $n (float)
 Denominator: $d (int)
 --}}
 
 <?php
-  $fullStars = floor($n);
-  $halfStars = ceil($n) == floor($n) ? 0:1;
-  $emptyStars = $halfStars? $d - 1 - $fullStars : $d - $fullStars;
+  // convert score to a 5 star rating system
+  $ratio = $n / $d ;
+
+  $score_over_five = $ratio * 5;
+
+  // round it to the nearest 0.5 ;
+  $score_over_five = (round($score_over_five * 2))/2;
+
+  $N = $score_over_five;
+  $D = 5;
+
+  $fullStars = floor($N);
+  $halfStars = ceil($N) == floor($N) ? 0:1;
+  $emptyStars = $halfStars? $D - 1 - $fullStars : $D - $fullStars;
   $score = $fullStars;
   if ($halfStars) {
     $score = $score + 0.5;
   }
 ?>
 
-<div class="stars" title="Blogger rating: {{$score}} / {{$d}}">
+<div class="stars" title="Blogger rating: {{$score}} / {{$D}} (original: {{$n}} / {{$d}})">
 
   <?php
     for ($i=0; $i < $fullStars ; $i++) {
