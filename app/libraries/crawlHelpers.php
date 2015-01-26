189fe2d54971c33d45aee2c66ce338b1d98c1665
Mustapha <mustapha.hamoui@gmail.com>
2015-01-26 21:50:52 +0000

    fixed crawler to scrape images that are refered to by ../path/to/image

--- app/libraries/crawlHelpers.php
+++ app/libraries/crawlHelpers.php
@@ -101,8 +101,10 @@ class crawlHelpers extends BaseController
           // if the image is relative, convert it to absolute
           if ($tmpImage[0] == '/') { // root url, example ('/src/of/image.jpg')
             $tmpImage = $root . $tmpImage;
+          } elseif (strpos($tmpImage, '../') == 0){ // also root ('../src/of/image')
+            $tmpImage = $root . '/' . substr($tmpImage,3);
           } elseif (strpos($tmpImage, 'http') != 0) { // relative url, example('src/of/image.jpg')
-            $tmpImage = $root. '/' . $tmpImage;
+            $tmpImage = $root . '/' . $tmpImage;
           }
           echo $root . "\n";
           echo $tmpImage . "\n";
