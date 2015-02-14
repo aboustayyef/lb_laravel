89f970e001daef4884dfbc0805a27197cd916f3c
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2015-02-14 17:38:17 +0000

    bugfix

--- app/views/static/edit/post.blade.php
+++ app/views/static/edit/post.blade.php
@@ -66,8 +66,8 @@
   {{ Form::label('excerpt', 'Post Excerpt')}}
   {{ Form::textarea('excerpt', $post->post_excerpt) }}
 
-  {{ Form::label('rating', 'If This is a review, enter your rating (over 5')}}
-  <input name="rating" type="text" value="{{$post->rating_numerator}}" id="rating" style="width:60px">
+  {{ Form::label('rating', 'If This is a review, enter your rating (over 5)')}}
+  <input name="rating" type="text" value="{{$post->rating_numerator}}" id="rating" style="width:60px"> <br>
 
 @if ($post->post_image_height > 0 )
   <img src="{{$post->post_image}}" width="300" alt="your post's image">
