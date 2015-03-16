b7ce2591d7dd76bea92ca6febe5724df9b156192
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2015-03-16 20:05:31 +0000

    added helper app to news crawling to fix urls with arabic paths

---
+++ app/lebaneseblogs/crawling/news/Helpers.php
@@ -0,0 +1,29 @@
+<?php namespace LebaneseBlogs\Crawling\News ;
+
+  class Helpers
+  {
+
+    /**
+     * Better url_encode for links with arabic characters
+     *
+     * @param   string $link , the url to be fixed
+     * @return  string $fixedLink,the fixed url
+     */
+
+    static function fixLink($link){
+
+      $url_parts = parse_url($link);
+      $part_to_encode = $url_parts['path'];
+      $encoded = urlencode($part_to_encode);
+      echo "encoded before pregreplace: $encoded \n";
+      $encoded = preg_replace('#\%2F#','/', $encoded);
+      $encoded = preg_replace('#\_#', '%84', $encoded);
+      echo "encoded after pregreplace: $encoded \n";
+      $fixedLink = $url_parts['scheme'] . '://' . $url_parts['host'] . $encoded;
+      return $fixedLink;
+
+    }
+
+  }
+
+?>
