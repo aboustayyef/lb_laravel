cf59621a386e729ebefd202e08c73000e4688656
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2015-05-05 22:18:14 +0000

    refinement in mobile css, centering ads and increase top tweet header breathing space

--- app/views/posts/extras/adsense1.blade.php
+++ app/views/posts/extras/adsense1.blade.php
@@ -1,14 +1,16 @@
 <div class="post_wrapper no_min">
-  <div class="card no_color no_border no_min" style="height:250px;width:300px">
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <!-- Tips Substitute -->
-    <ins class="adsbygoogle"
-       style="display:inline-block;width:300px;height:250px"
-       data-ad-client="ca-pub-7449692210953130"
-       data-ad-slot="2395550378">
-    </ins>
-    <script>
-      (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
+  <div class="card no_color no_border no_min" >
+    <div style="width:300px; margin:0 auto;"> {{-- Used to center ads --}}
+      <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- Tips Substitute -->
+        <ins class="adsbygoogle"
+           style="display:inline-block;width:300px;height:250px"
+           data-ad-client="ca-pub-7449692210953130"
+           data-ad-slot="2395550378">
+        </ins>
+        <script>
+          (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+      </div>
   </div>
 </div>
