898130d651d613033bdd31a5e761dbb7666cec5c
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2015-06-27 09:00:40 +0000

    try to fix overflow issue

--- app/views/mobile/header.blade.php
+++ app/views/mobile/header.blade.php
@@ -43,7 +43,7 @@
     <link rel="apple-touch-icon" sizes="180x180" href="{{asset('/img/apple-touch-icons/180x180.png')}}">
 
     <!-- Style Sheet -->
-    <link rel="stylesheet" href="{{asset('/css/mobile.css?v=1.51')}}">
+    <link rel="stylesheet" href="{{asset('/css/mobile.css?v=1.52')}}">
 
     <!-- Favicon -->
     <link rel="shortcut icon" href="{{asset('/img/favicon.ico')}}" >
