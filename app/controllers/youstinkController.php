d371f2275df678afcb985aee6669d1a81a071df1
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2015-09-04 21:05:04 +0000

    improved youstink api

--- app/controllers/youstinkController.php
+++ app/controllers/youstinkController.php
@@ -10,7 +10,10 @@ class YoustinkController extends \BaseController {
 	 */
 	public function index($howmany=5)
 	{
-		$posts = Post::with('blog')->where('post_content', 'like', '%#youstink%')->orderBy('post_timestamp','desc')->take($howmany)->get();
+		$posts = Post::with('blog')->where('post_content', 'like', '%#youstink%')
+    ->orWhere('post_content', 'like', '%you stink%')
+    ->orWhere('post_content', 'like', '%طلعت ريحتكم%')
+    ->orderBy('post_timestamp','desc')->take($howmany)->get();
     return View::make('youstink')->with(compact('posts'));
 	}
 
