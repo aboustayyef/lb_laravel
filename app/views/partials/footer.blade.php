7626df17839361c078979c975aca026f83db01d4
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2015-11-28 15:37:11 +0000

    removed statcounter

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -27,23 +27,6 @@
               ga('require', 'displayfeatures');
               ga('send', 'pageview');
             </script>
-
-      <!-- Statcounter -->
-          <script type="text/javascript">
-          var sc_project=8489889;
-          var sc_invisible=1;
-          var sc_security="6ec3dc93";
-          var scJsHost = (("https:" == document.location.protocol) ?
-          "https://secure." : "http://www.");
-          document.write("<sc"+"ript type='text/javascript' src='" +
-          scJsHost +
-          "statcounter.com/counter/counter.js'></"+"script>");</script>
-          <noscript><div class="statcounter"><a title="web counter"
-          href="http://statcounter.com/" target="_blank"><img
-          class="statcounter"
-          src="https://c.statcounter.com/8489889/0/6ec3dc93/1/"
-          alt="web counter"></a></div></noscript>
-      <!-- End of StatCounter Code for Default Guide -->
     </div>
 </body>
 </html>
