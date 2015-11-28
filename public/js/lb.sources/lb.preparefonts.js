9ee9a2c094cf574a61bbd688bfa2cf5ce161f815
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2015-11-28 21:16:04 +0000

    more optimization. Discarding web fonts

--- public/js/lb.sources/lb.preparefonts.js
+++ public/js/lb.sources/lb.preparefonts.js
@@ -1,26 +1,30 @@
-$(document).ready(function() {
-  console.log('trying to load fonts');
-  WebFontConfig = {
-    google: { families: [ 'Montserrat', 'Droid Arabic Naskh' ] },
-
-    // when the fonts download, beging masonry layout..
-    fontactive: function(fontFamily, fontDescription) {
-      console.log('WebFonts have loaded');
-      lbApp.veryFirstLoad();
-    },
-    inactive: function() {
-      console.log('WebFonts have Failed to load, need to move on with normal fonts');
-      lbApp.veryFirstLoad();
-    }
-  };
-  (function() {
-    var wf = document.createElement('script');
-    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-    wf.type = 'text/javascript';
-    wf.async = 'true';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(wf, s);
-  })();
-
+$(document).ready(function(){
+  lbApp.veryFirstLoad();
 });
+
+// $(document).ready(function() {
+//   console.log('trying to load fonts');
+//   WebFontConfig = {
+//     google: { families: [ 'Montserrat', 'Droid Arabic Naskh' ] },
+
+//     // when the fonts download, beging masonry layout..
+//     fontactive: function(fontFamily, fontDescription) {
+//       console.log(fontFamily + ' has loaded');
+//       //lbApp.veryFirstLoad();
+//     },
+//     inactive: function() {
+//       console.log('WebFonts have Failed to load, need to move on with normal fonts');
+//       lbApp.veryFirstLoad();
+//     }
+//   };
+//   (function() {
+//     var wf = document.createElement('script');
+//     wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+//       '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+//     wf.type = 'text/javascript';
+//     wf.async = 'true';
+//     var s = document.getElementsByTagName('script')[0];
+//     s.parentNode.insertBefore(wf, s);
+//   })();
+
+// });
--- public/js/lb.sources/min/lb.showPost-min.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){lb.showPosts=function(o){console.log(o)}});
