d9fef97374d3b09a7d4c76050aa033f96c9154c9
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2016-06-12 07:43:19 +0000

    cleanup work

--- app/controllers/youstinkController.php
+++ app/controllers/youstinkController.php
@@ -2,35 +2,20 @@
 
 class YoustinkController extends \BaseController {
 
-	/**
-	 * Display a listing of the resource.
-	 * GET /youstink
-	 *
-	 * @return Response
-	 */
-	public function index($howmany=5)
-	{
-		$posts = Post::with('blog')->where('post_content', 'like', '%youstink%')
-    ->orWhere('post_content', 'like', '%you stink%')
-    ->orWhere('post_content', 'like', '%طلعت ريحتكم%')
-    ->orderBy('post_timestamp','desc')->take($howmany)->get();
+    /**
+     * Display a listing of the resource.
+     * GET /youstink
+     *
+     * @return Response
+     */
 
-    return View::make('youstink')->with(compact('posts'));
-	}
+    public function index($howmany=5)
+    {
+        $posts = Post::with('blog')->where('post_content', 'like', '%youstink%')
+            ->orWhere('post_content', 'like', '%you stink%')
+            ->orWhere('post_content', 'like', '%طلعت ريحتكم%')
+            ->orderBy('post_timestamp','desc')->take($howmany)->get();
 
-  public function index2($howmany=5)
-  {
-    $posts = Post::with('blog')
-    ->where('post_content', 'like', '%youstink%')
-    ->orWhere('post_content', 'like', '%you stink%')
-    ->orWhere('post_content', 'like', '%طلعت ريحتكم%')
-    ->orWhere('post_content', 'like', '%garbage%')
-    ->where('post_title', 'like', '%youstink%')
-    ->orWhere('post_title', 'like', '%you stink%')
-    ->orWhere('post_title', 'like', '%طلعت ريحتكم%')
-    ->orderBy('post_timestamp','desc')->take($howmany)->get();
-
-    return $posts;
-  }
-
+        return View::make('youstink')->with(compact('posts'));
+    }
 }
--- app/routes.php
+++ app/routes.php
@@ -9,117 +9,116 @@ use Symfony\Component\DomCrawler\Crawler ;
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the Closure to execute when that URI is requested.
 |
-*/
+ */
 
 // The default route. Check url 'channel' parameter for backward compatibility 
 // Default entrypoint is /posts/all
 
 Route::get('/', function(){
-  if (Input::has('channel')) {
-    return Redirect::to('/posts/'.Input::get('channel'));
-  }
-  return Redirect::to('posts/all');
+    if (Input::has('channel')) {
+        return Redirect::to('/posts/'.Input::get('channel'));
+    }
+    return Redirect::to('posts/all');
 });
 
 // Get a list of #youstink related posts
 // An api hack for youstink.news
- 
+
 Route::get('/youstink2/{howmany?}', array(
-  'as'  =>  'youstink',
-  'uses'  =>  'youstinkController@index'
+    'as'  =>  'youstink',
+    'uses'  =>  'youstinkController@index'
 ));
 
 // Get a list of all the sources.
 // Useful for importing to another database. Used for LBEngine
 
 Route::get('/sources/{password}', function($password){
-  if (Hash::check($password, '$2y$10$6PHNiZP68bq2KPF9QGzVN.2VrWbWYNEowZPgcIaKpisMVfmYjlxbm')) {
-    return Blog::all();
-  }
+    if (Hash::check($password, '$2y$10$6PHNiZP68bq2KPF9QGzVN.2VrWbWYNEowZPgcIaKpisMVfmYjlxbm')) {
+        return Blog::all();
+    }
 });
 
 Route::group(array('prefix' => 'admin', 'before' => 'admin.auth'), function()
-{
-    Route::get('addBlog',array(
-      'as'  =>  'admin.getAddBlog',
-      'uses' => 'adminController@getstep1'
-    ));
+    {
+        Route::get('addBlog',array(
+            'as'  =>  'admin.getAddBlog',
+            'uses' => 'adminController@getstep1'
+        ));
 
-    Route::get('addBlog/step2',array(
-      'as'  =>  'admin.getstep2',
-      'uses' => 'adminController@getstep2'
-    ));
+        Route::get('addBlog/step2',array(
+            'as'  =>  'admin.getstep2',
+            'uses' => 'adminController@getstep2'
+        ));
 
-    Route::post('addBlog/step2',array(
-      'as'  =>  'admin.storeBlog',
-      'uses' => 'adminController@store'
-    ));
+        Route::post('addBlog/step2',array(
+            'as'  =>  'admin.storeBlog',
+            'uses' => 'adminController@store'
+        ));
 
-});
-
+    });
 
 Route::get('/mobileAjax/b/{blogger}/{from}/{howmany}', array(
-  'uses'  =>  'MobileAjaxController@bloggerIndex'
+    'uses'  =>  'MobileAjaxController@bloggerIndex'
 ));
 
 Route::get('/mobileAjax/{channel}/{from}/{howmany}', array(
-  'uses'  =>  'MobileAjaxController@index'
+    'uses'  =>  'MobileAjaxController@index'
 ));
 
 Route::get('/news', array(
-  'uses'  =>  'NewsController@index'
+    'uses'  =>  'NewsController@index'
 ));
 
 Route::get('/user/{section}/{action?}', array(
-  'before'  =>  'lb.auth:following',
-  'as'  =>  'user',
-  'uses'  =>  'UserController@index'
+    'before'  =>  'lb.auth:following',
+    'as'  =>  'user',
+    'uses'  =>  'UserController@index'
 ));
 
 Route::get('mobile/{set}/{detail?}', array(
-  'as'  =>  'mobile',
-  'uses'  =>  'MobileController@index'
+    'as'  =>  'mobile',
+    'uses'  =>  'MobileController@index'
 ));
 
 Route::get('/posts/{channel}/{action?}', array(
-  'as'  =>  'posts',
-  'uses'  =>  'PostsController@index'
+    'as'  =>  'posts',
+    'uses'  =>  'PostsController@index'
 ));
 
 // redirect outgoing links, for exit link counting and registering
 Route::get('exit', array(
-  'uses'  => 'ExitController@lbExit'
+    'uses'  => 'ExitController@lbExit'
 ));
 
 Route::get('posts/search', array(
-  'as'      =>  'search',
-  'uses'    =>  'PostsController@search'
+    'as'      =>  'search',
+    'uses'    =>  'PostsController@search'
 ));
 
 Route::get('/blogger/{nameId?}', array(
-  'as'    =>  'blogger',
-  'uses'  =>  'BloggerController@showPosts'
+    'as'    =>  'blogger',
+    'uses'  =>  'BloggerController@showPosts'
 ));
 
 Route::get('/edit/{what?}/{which?}', array(
-  'uses'  =>  'EditController@index'
+    'uses'  =>  'EditController@index'
 ));
 
 Route::post('/edit/{what?}/{which?}', array(
-  'before'  =>  'csrf',
-  'uses'  =>  'EditController@submit'
+    'before'  =>  'csrf',
+    'uses'  =>  'EditController@submit'
 ));
 
 Route::get('/delete/{what?}/{which?}', array(
-  'uses'  =>  'DeleteController@index'
+    'uses'  =>  'DeleteController@index'
 ));
 
 Route::get('/ajax/GetMorePosts', array(
-  'uses'  =>  'AjaxController@loadMorePosts'
+    'uses'  =>  'AjaxController@loadMorePosts'
 ));
 
 Route::get('/ajax/GetTop5', array(
-  'uses'  =>  'AjaxController@loadTopFivePosts'
+    'uses'  =>  'AjaxController@loadTopFivePosts'
 ));
 
 /*
@@ -129,14 +128,14 @@ Route::get('/ajax/GetTop5', array(
 |
 |   destroys cookies and sessions
 |
-*/
+ */
 
 Route::get('/logout',function()
-{
-  Session::forget('lb_user_id');
-  $cookie = Cookie::forget('lb_user_id');
-  return Redirect::to('/')->withCookie($cookie);
-});
+    {
+        Session::forget('lb_user_id');
+        $cookie = Cookie::forget('lb_user_id');
+        return Redirect::to('/')->withCookie($cookie);
+    });
 
 
 /*
@@ -144,17 +143,17 @@ Route::get('/logout',function()
 | Static routes for the "about pages"
 |--------------------------------------------------------------------------
 | These are the routes to take us to static pages
-*/
+ */
 
 Route::get('/about/{slug?}', array(
-  'as'    =>  'staticpages',
-  'uses'  =>  'StaticPagesController@index'
+    'as'    =>  'staticpages',
+    'uses'  =>  'StaticPagesController@index'
 ));
 
 Route::post('/about/{slug?}', array(
-  'before'  =>  'csrf',
-  'as'  =>  'submition',
-  'uses'  =>  'StaticPagesController@submit'
+    'before'  =>  'csrf',
+    'as'  =>  'submition',
+    'uses'  =>  'StaticPagesController@submit'
 ));
 
 
@@ -163,29 +162,29 @@ Route::post('/about/{slug?}', array(
 |   The Login Page
 |---------------------------------------------------------------------
 |
-*/
+ */
 
 Route::get('/login',function()
-{
-    if (Input::has('follow')) {
-      Session::set('blogToFollow', Input::get('follow'));
-     }
-    if (Input::has('like')) {
-      Session::set('postToLike', Input::get('like'));
-     }
-    if (Input::has('camefrom')){
-      Session::set('finalDestination', Input::get('camefrom'));
-    }
-    return View::make('login');
-});
+    {
+        if (Input::has('follow')) {
+            Session::set('blogToFollow', Input::get('follow'));
+        }
+        if (Input::has('like')) {
+            Session::set('postToLike', Input::get('like'));
+        }
+        if (Input::has('camefrom')){
+            Session::set('finalDestination', Input::get('camefrom'));
+        }
+        return View::make('login');
+    });
 
 /**
  * Blog Routes. This will redirect old blog urls based on subfolder to new subdomain.
  */
 
 Route::get('/blog/{slug1?}/{slug2?}/{slug3?}/{slug4?}', function($slug1=null, $slug2=null, $slug3=null, $slug4=null){
-  $endSlug = '/'. $slug1 .'/'. $slug2 .'/'. $slug3 .'/'. $slug4;
-  return Redirect::to('http://blog.lebaneseblogs.com'.$endSlug);
+    $endSlug = '/'. $slug1 .'/'. $slug2 .'/'. $slug3 .'/'. $slug4;
+    return Redirect::to('http://blog.lebaneseblogs.com'.$endSlug);
 });
 
 
@@ -196,14 +195,14 @@ Route::get('/blog/{slug1?}/{slug2?}/{slug3?}/{slug4?}', function($slug1=null, $s
 |
 |   Use to authenticate with third party providers
 |
-*/
+ */
 
 Route::get('/auth/{provider}', array(
-  'uses'  =>  'AuthenticationController@auth'
+    'uses'  =>  'AuthenticationController@auth'
 ));
 
 Route::get('/auth/{provider}/callback', array(
-  'uses'  =>  'AuthenticationController@callback'
+    'uses'  =>  'AuthenticationController@callback'
 ));
 
 /*
@@ -213,5 +212,5 @@ Route::get('/auth/{provider}/callback', array(
 |   examples:
 |   lebaneseblogs.com/beirutspring -> lebaneseblogs.com/blogger/beirutspring
 |   lebaneseblogs.com/fashion -> lebanesbelogs.com/channel/fashion
-*/
+ */
 Route::get('{slug}', ['uses'  =>  'UrlController@redirect']);
