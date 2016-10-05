dd500dc80124c05d87bb10829c8842fdeec1d097
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2016-10-05 19:37:30 +0000

    Migration to add unique ID to each post from RSS

---
+++ app/database/migrations/2016_10_04_170044_add_post_rss_id_to_posts.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddPostRssIdToPosts extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('posts', function(Blueprint $table)
+		{
+			$table->string('post_rss_id')->nullable();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('posts', function(Blueprint $table)
+		{
+			$table->dropColumn('post_rss_id');
+		});
+	}
+
+}
